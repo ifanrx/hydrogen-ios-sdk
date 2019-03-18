8967fc73b02a8a0a56ac16581895c5c0fa860dd2
pqhifanr <pengquanhua@ifanr.com>
2019-03-18 11:56:53 +0800

    Initial Commit

---
+++ BaaSSDK/BaaSSDK.h
@@ -0,0 +1,19 @@
+//
+//  BaaSSDK.h
+//  BaaSSDK
+//
+//  Created by pengquanhua on 2019/3/18.
+//  Copyright © 2019 ifanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BaaSSDK.
+FOUNDATION_EXPORT double BaaSSDKVersionNumber;
+
+//! Project version string for BaaSSDK.
+FOUNDATION_EXPORT const unsigned char BaaSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaaSSDK/PublicHeader.h>
+
+
